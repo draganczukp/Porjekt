f0b2ca6c86482910bb828800960aab24281a37ba
Przemek Dragańczuk <draganczukp@gmail.com>
2020-01-26 16:17:13 +0100

    Koniec

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -8,25 +8,49 @@ using System.Linq;
 
 public class GameState : MonoBehaviour
 {
+    [Header("UI")]
+    [Tooltip("UI Text object for displaying points")]
     public Text scoreText;
-    public Transform levelParent;
+    [Tooltip("UI Text object for displaying time elapsed in current level")]
+    public Text timeText;
 
+    [Header("Levels")]
+    [Tooltip("GameObject to which the levels are attached to")]
+    public Transform levelParent;
+    [Tooltip("Reference to current level")]
     public GameObject currentLevel;
+    [Tooltip("Array of Prefabs with levels")]
     public List<GameObject> levels;
 
+    // Current level
     private int levelNumber;
 
+    // Points collected in previous levels
     private int oldPoints = 0;
 
+    // List of collected points. Used for resetting points on death
     private List<GameObject> points = new List<GameObject>();
+
+    private float levelStartTime;
+
+    void Start()
+    {
+        levelStartTime = Time.time;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
 
     void Update()
     {
-        scoreText.text = $"Points: {points.Count + oldPoints}";
+        int totalPoints = points.Count + oldPoints;
+        scoreText.text = $"Points: {totalPoints}";
+        
+        float elapsedTime = Time.time - levelStartTime;
+        timeText.text = $"Time: {elapsedTime.ToString("00.0000")}";
 
-		Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;
-
+        // Pressing Escpe exits the game
         if (Input.GetKey(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
@@ -40,12 +64,14 @@ public class GameState : MonoBehaviour
 
     }
 
+    // Executed when player collides with a point pickup
     public void OnPickupCollision(GameObject gameObject)
     {
         gameObject.SetActive(false);
         points.Add(gameObject);
     }
 
+    // Executed when player falls out of bounds and dies
     public void OnPlayerDeath()
     {
         foreach (GameObject o in points)
@@ -55,16 +81,25 @@ public class GameState : MonoBehaviour
         points.Clear();
     }
 
+    // Runs when player touches the level end marker
     public void OnNextLevel()
     {
         oldPoints += points.Count;
+
         points.Clear();
+
         Destroy(currentLevel);
+
         currentLevel = Instantiate(levels[levelNumber], levelParent);
+
         levelNumber = (levelNumber + 1) % levels.Count();
+
         currentLevel.SetActive(true);
+
+        levelStartTime = Time.time;
     }
 
+    // Returns the position of player starting point
     public Transform GetStartPoint()
     {
         var transforms = from transform in currentLevel.GetComponentsInChildren<Transform>()
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,124 +4,110 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-
-struct Cmd
-{
-    public float forwardMove;
-    public float rightMove;
-    public float upMove;
-}
-
-
+[RequireComponent(typeof(CharacterController), typeof(BoxCollider))]
 public class PlayerController : MonoBehaviour
 {
     public Transform startPosition;
 
-    public Transform playerView;     // Camera
-    public float playerViewYOffset = 0.6f; // The height at which the camera is bound to
-    public float xMouseSensitivity = 30.0f;
+    public Transform cameraTransform;
+
+    public float playerViewYOffset = 0.6f;
     public float yMouseSensitivity = 30.0f;
-    //
-    /*Frame occuring factors*/
+    public float xMouseSensitivity = 30.0f;
+    private float sensitivityMultiplier = .2f;
+
+
     public float gravity = 20.0f;
 
-    public float friction = 6; //Ground friction
+    public float friction = 6;
 
-    /* Movement stuff */
-    public float moveSpeed = 7.0f;                // Ground move speed
-    public float runAcceleration = 14.0f;         // Ground accel
-    public float runDeacceleration = 10.0f;       // Deacceleration that occurs when running on the ground
-    public float airAcceleration = 2.0f;          // Air accel
-    public float airDecceleration = 2.0f;         // Deacceleration experienced when ooposite strafing
-    public float airControl = 0.3f;               // How precise air control is
-    public float sideStrafeAcceleration = 50.0f;  // How fast acceleration occurs to get up to sideStrafeSpeed when
-    public float sideStrafeSpeed = 1.0f;          // What the max speed to generate when side strafing
-    public float jumpSpeed = 8.0f;                // The speed at which the character's up axis gains when hitting jump
-    public bool holdJumpToBhop = false;
+    public float moveSpeed = 7.0f;
+    public float runAcceleration = 14.0f;
+    public float runDeacceleration = 10.0f;
 
-    private CharacterController _controller;
-
-    // Camera rotations
-    private float rotX = 0.0f;
-    private float rotY = 0.0f;
-
-    private Vector3 moveDirectionNorm = Vector3.zero;
-    private Vector3 playerVelocity = Vector3.zero;
-    private float playerTopVelocity = 0.0f;
-
-    // Q3: players can queue the next jump just before he hits the ground
-    private bool wishJump = false;
-
-    // Used to display real time fricton values
-    private float playerFriction = 0.0f;
-
-    private Cmd _cmd;
+    public float airAcceleration = 2.0f;
+    public float airDecceleration = 2.0f;
+    public float airControl = 0.3f;
+    public float sideStrafeAcceleration = 50.0f;
+    public float sideStrafeSpeed = 1.0f;
+    public float jumpSpeed = 8.0f;
 
     public GameState GameState;
 
+    private float moveForward;
+    private float moveSideways;
+    private CharacterController characterController;
+
+    private float rotY = 0.0f;
+    private float rotX = 0.0f;
+
+    private Vector3 moveDirectionNorm = Vector3.zero;
+    private Vector3 playerVelocity = Vector3.zero;
+    private bool wishJump = false;
+
+
     private void Start()
     {
-        if (playerView == null)
-        {
-            Camera mainCamera = Camera.main;
-            if (mainCamera != null)
-                playerView = mainCamera.gameObject.transform;
-        }
+        transform.position = startPosition.position;
 
-        transform.position.Set(startPosition.position.x, startPosition.position.y, startPosition.position.z);
-
-        // Put the camera inside the capsule collider
-        playerView.position = new Vector3(
+        cameraTransform.position = new Vector3(
                 transform.position.x,
                 transform.position.y + playerViewYOffset,
                 transform.position.z);
 
-        _controller = GetComponent<CharacterController>();
+        characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
-        /* Camera rotation stuff, mouse controls this shit */
-        rotX += Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * .2f;
-        rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * .2f;
+        RotateCamera();
 
-        // Clamp the X rotation
-        if (rotX < -90)
-            rotX = -90;
-        else if (rotX > 90)
-            rotX = 90;
-
-        this.transform.rotation = Quaternion.Euler(0, rotY, 0); // Rotates the collider
-        playerView.rotation = Quaternion.Euler(rotX, rotY, 0); // Rotates the camera
-
-        /* Movement, here's the important part */
         QueueJump();
-        if (_controller.isGrounded)
+
+        HandleMovement();
+
+        UpdateCamera();
+
+        CheckOutOfBounds();
+    }
+
+    private void UpdateCamera()
+    {
+        cameraTransform.position = new Vector3(
+                        transform.position.x,
+                        transform.position.y + playerViewYOffset,
+                        transform.position.z);
+    }
+
+    private void HandleMovement()
+    {
+        if (characterController.isGrounded)
             GroundMove();
-        else if (!_controller.isGrounded)
+        else if (!characterController.isGrounded)
             AirMove();
 
-        // Move the controller
-        _controller.Move(playerVelocity * Time.deltaTime);
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
 
-        /* Calculate top velocity */
-        Vector3 udp = playerVelocity;
-        udp.y = 0.0f;
-        if (udp.magnitude > playerTopVelocity)
-            playerTopVelocity = udp.magnitude;
-
-        playerView.position = new Vector3(
-                transform.position.x,
-                transform.position.y + playerViewYOffset,
-                transform.position.z);
-
-
+    private void CheckOutOfBounds()
+    {
         if (transform.position.y < -20)
         {
             transform.position = new Vector3(startPosition.position.x, startPosition.position.y, startPosition.position.z);
             playerVelocity = Vector3.zero;
             GameState.OnPlayerDeath();
         }
+    }
+
+    private void RotateCamera()
+    {
+        rotY += Input.GetAxisRaw("Mouse Y") * yMouseSensitivity * sensitivityMultiplier;
+        rotX += Input.GetAxisRaw("Mouse X") * xMouseSensitivity * sensitivityMultiplier;
+
+        rotY = Mathf.Clamp(rotY, -90, 90);
+
+        this.transform.rotation = Quaternion.Euler(0, rotX, 0);
+        cameraTransform.rotation = Quaternion.Euler(rotY, rotX, 0);
     }
 
     void OnTriggerEnter(Collider other)
@@ -141,22 +127,13 @@ public class PlayerController : MonoBehaviour
 
     private void SetMovementDir()
     {
-        _cmd.forwardMove = Input.GetAxisRaw("Vertical");
-        _cmd.rightMove = Input.GetAxisRaw("Horizontal");
+        moveForward = Input.GetAxisRaw("Vertical");
+        moveSideways = Input.GetAxisRaw("Horizontal");
     }
 
     private void QueueJump()
     {
-        if (holdJumpToBhop)
-        {
-            wishJump = Input.GetButton("Jump");
-            return;
-        }
-
-        if (Input.GetButtonDown("Jump") && !wishJump)
-            wishJump = true;
-        if (Input.GetButtonUp("Jump"))
-            wishJump = false;
+        wishJump = Input.GetButton("Jump");
     }
 
     /**
@@ -170,7 +147,7 @@ public class PlayerController : MonoBehaviour
 
         SetMovementDir();
 
-        wishdir = new Vector3(_cmd.rightMove, 0, _cmd.forwardMove);
+        wishdir = new Vector3(moveSideways, 0, moveForward);
         wishdir = transform.TransformDirection(wishdir);
 
         float wishspeed = wishdir.magnitude;
@@ -179,14 +156,14 @@ public class PlayerController : MonoBehaviour
         wishdir.Normalize();
         moveDirectionNorm = wishdir;
 
-        // CPM: Aircontrol
+
         float wishspeed2 = wishspeed;
         if (Vector3.Dot(playerVelocity, wishdir) < 0)
             accel = airDecceleration;
         else
             accel = airAcceleration;
-        // If the player is ONLY strafing left or right
-        if (_cmd.forwardMove == 0 && _cmd.rightMove != 0)
+
+        if (moveForward == 0 && moveSideways != 0)
         {
             if (wishspeed > sideStrafeSpeed)
                 wishspeed = sideStrafeSpeed;
@@ -196,38 +173,31 @@ public class PlayerController : MonoBehaviour
         Accelerate(wishdir, wishspeed, accel);
         if (airControl > 0)
             AirControl(wishdir, wishspeed2);
-        // !CPM: Aircontrol
 
-        // Apply gravity
+
+
         playerVelocity.y -= gravity * Time.deltaTime;
     }
 
-    /**
-	 * Air control occurs when the player is in the air, it allows
-	 * players to move side to side much faster rather than being
-	 * 'sluggish' when it comes to cornering.
-	 */
+
     private void AirControl(Vector3 wishdir, float wishspeed)
     {
-        float zspeed;
-        float speed;
-        float dot;
-        float k;
-
+       
+       
         // Can't control movement if not moving forward or backward
-        if (Mathf.Abs(_cmd.forwardMove) < 0.001 || Mathf.Abs(wishspeed) < 0.001)
+        if (Mathf.Abs(moveForward) < 0.001 || Mathf.Abs(wishspeed) < 0.001)
             return;
-        zspeed = playerVelocity.y;
+
+        float zspeed = playerVelocity.y;
         playerVelocity.y = 0;
-        /* Next two lines are equivalent to idTech's VectorNormalize() */
-        speed = playerVelocity.magnitude;
+
+        float speed = playerVelocity.magnitude;
         playerVelocity.Normalize();
 
-        dot = Vector3.Dot(playerVelocity, wishdir);
-        k = 32;
+        float dot = Vector3.Dot(playerVelocity, wishdir);
+        float k = 32;
         k *= airControl * dot * dot * Time.deltaTime;
 
-        // Change direction while slowing down
         if (dot > 0)
         {
             playerVelocity.x = playerVelocity.x * speed + wishdir.x * k;
@@ -239,7 +209,7 @@ public class PlayerController : MonoBehaviour
         }
 
         playerVelocity.x *= speed;
-        playerVelocity.y = zspeed; // Note this line
+        playerVelocity.y = zspeed;
         playerVelocity.z *= speed;
     }
 
@@ -250,15 +220,14 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 wishdir;
 
-        // Do not apply friction if the player is queueing up the next jump
-        if (!wishJump)
-            ApplyFriction(1.0f);
-        else
+        if (wishJump)
             ApplyFriction(0);
+        else
+            ApplyFriction(1.0f);
 
         SetMovementDir();
 
-        wishdir = new Vector3(_cmd.rightMove, 0, _cmd.forwardMove);
+        wishdir = new Vector3(moveSideways, 0, moveForward);
         wishdir = transform.TransformDirection(wishdir);
         wishdir.Normalize();
         moveDirectionNorm = wishdir;
@@ -278,30 +247,22 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    /**
-	 * Applies friction to the player, called in both the air and on the ground
-	 */
     private void ApplyFriction(float t)
     {
-        Vector3 vec = playerVelocity; // Equivalent to: VectorCopy();
-        float speed;
-        float newspeed;
-        float control;
-        float drop;
-
+        Vector3 vec = playerVelocity;
         vec.y = 0.0f;
-        speed = vec.magnitude;
-        drop = 0.0f;
+        float speed = vec.magnitude;
+        float drop = 0.0f;
 
-        /* Only if the player is on the ground then apply friction */
-        if (_controller.isGrounded)
+
+        if (characterController.isGrounded)
         {
-            control = speed < runDeacceleration ? runDeacceleration : speed;
+            float control = speed < runDeacceleration ? runDeacceleration : speed;
             drop = control * friction * Time.deltaTime * t;
         }
 
-        newspeed = speed - drop;
-        playerFriction = newspeed;
+        float newspeed = speed - drop;
+
         if (newspeed < 0)
             newspeed = 0;
         if (speed > 0)
@@ -313,15 +274,11 @@ public class PlayerController : MonoBehaviour
 
     private void Accelerate(Vector3 wishdir, float wishspeed, float accel)
     {
-        float addspeed;
-        float accelspeed;
-        float currentspeed;
-
-        currentspeed = Vector3.Dot(playerVelocity, wishdir);
-        addspeed = wishspeed - currentspeed;
+        float currentspeed = Vector3.Dot(playerVelocity, wishdir);
+        float addspeed = wishspeed - currentspeed;
         if (addspeed <= 0)
             return;
-        accelspeed = accel * Time.deltaTime * wishspeed;
+        float accelspeed = accel * Time.deltaTime * wishspeed;
         if (accelspeed > addspeed)
             accelspeed = addspeed;
 
